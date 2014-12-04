13ed5e6ed5194dac848c0c8bfaf7fd56fa299206
bishopblade <rj7300@gmail.com>
2014-12-04 11:01:17 -0800

    Cleaned up big if/else statement in sign in class

--- src/tk/aegisstudios/traveltracker/SignInActivity.java
+++ src/tk/aegisstudios/traveltracker/SignInActivity.java
@@ -48,8 +48,7 @@ public class SignInActivity extends Activity {
         @Override
         protected void onPostExecute(String result) {
             String signToastMessage;
-            
-            if (result.split(";").length > 1 && result.split(";")[0].equals("Authenticated")) {
+            if (result.split(";").length > 1) {
             	signToastMessage = "You succesfully logged in.";
                 FileOutputStream savedAuthStream = openAuthenticationData();
                 
@@ -57,20 +56,24 @@ public class SignInActivity extends Activity {
                 boolean authSaved = writeAuthenticationData(savedAuthStream, separated[1]);
                 
                 redirectToHome();
-            } else if (result.split(";")[0].equals("Wrong password")) {
-                signToastMessage = "You entered an incorrect password. "
-                        + "Please enter the correct one and try again.";
-            } else if (result.split(";")[0].equals("User does not exist")) {
-                signToastMessage = "Sorry, that user does not exist.";
-            } else if (result.split(";")[0].equals("Access error")) {
-                signToastMessage = "Sorry, we had a problem while accessing "
-                        + "the database. Please try again, and if the problem "
-                        + "pursues contact the developer.";
-            } else {
-                signToastMessage = "Unknown error code.";
+            } else if (result.split(";").length.equals(1)) {
+            	String returnVal = result.split(";")[0];
+            	switch (returnVal) {
+            	    case "Wrong password":
+            	    	signToastMessage = "Incorrect password entered";
+            	    	break;
+            	    case "User does not exist":
+            	    	signToastMessage = "User does not exist";
+            	    	break;
+            	    case "Access error":
+            	    	signToastMessage = "Error while accessing database";
+            	    	break;
+            	    default:
+            	    	signToastMessage = "Invalid server response";
+            	    	break;
+            	}
             }
-            Toast.makeText(getApplicationContext(), signToastMessage, 
-                    Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), signToastMessage, Toast.LENGTH_LONG).show();
         }
     }
 
